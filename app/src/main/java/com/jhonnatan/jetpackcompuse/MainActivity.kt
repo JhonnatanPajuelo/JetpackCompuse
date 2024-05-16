1f2499c6a37c35436b704761840d65d69adae2aa
Jhonnatan <83382553+JhonnatanPajuelo@users.noreply.github.com>
2024-05-15 20:44:46 -0500

    Box y sus atributos, centrar,scrool

--- app/src/main/java/com/jhonnatan/jetpackcompuse/MainActivity.kt
+++ app/src/main/java/com/jhonnatan/jetpackcompuse/MainActivity.kt
@@ -4,9 +4,16 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Surface
@@ -16,7 +23,9 @@ import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Devices
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -30,11 +39,10 @@ class MainActivity : ComponentActivity() {
             JetpackCompuseTheme {
                 //Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
                 Surface(
-
+                    modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
-                )
-                {
-                    ContadorAlPresionar()
+                ) {
+                    MyBox()
                 }
 
             }
@@ -42,19 +50,40 @@ class MainActivity : ComponentActivity() {
     }
 }
 
-
 @Composable
 fun ContadorAlPresionar() {
-    var conteo by remember{ mutableStateOf(0) }
+    var conteo by remember { mutableStateOf(0) }
     //Modifier me sirbe para agregarle nuevos atributos al elemento
     Text(text = "Conteo: $conteo".toString(),
         Modifier
             .clickable { conteo++ })
 }
+
+@Composable
+fun MyBox() {
+    Box(
+        Modifier
+            .fillMaxSize()
+            .background(Color.Red), contentAlignment = Alignment.Center
+    ) {
+        Box(
+            Modifier
+                .background(Color.Gray)
+                .width(50.dp)
+                .height(50.dp)
+                .verticalScroll(
+                    rememberScrollState()
+                )) {
+                    Text(text = "Hola soy un Focking Box")
+                }
+    }
+}
+
+
 @Preview(showBackground = true)
 @Composable
 fun GreetingPreview() {
     JetpackCompuseTheme {
-        ContadorAlPresionar()
+        MyBox()
     }
 }
