c1261d5762d3e155c31faeb35342a0d82a85162a
Jhonnatan <83382553+JhonnatanPajuelo@users.noreply.github.com>
2024-05-15 19:54:18 -0500

    Clickable ++, plantilla oficial

--- app/src/main/java/com/jhonnatan/jetpackcompuse/MainActivity.kt
+++ app/src/main/java/com/jhonnatan/jetpackcompuse/MainActivity.kt
@@ -4,6 +4,7 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.MaterialTheme
@@ -11,45 +12,49 @@ import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Devices
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.jhonnatan.jetpackcompuse.ui.theme.JetpackCompuseTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
+        //enableEdgeToEdge()
         setContent {
             JetpackCompuseTheme {
                 //Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
                 Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colorScheme.background)
-                    {
-                        MyText()
-                    }
 
+                    color = MaterialTheme.colorScheme.background
+                )
+                {
+                    ContadorAlPresionar()
                 }
+
             }
         }
     }
-//Elementos del preview
-@Preview (showBackground = true,name = "Name de my Preview", showSystemUi = true,apiLevel = 33, device = Devices.PIXEL)
-@Composable
-fun MyText(){
-    Text(text = "Hello Jhonnatan")
 }
 
+
 @Composable
-fun Greeting( ) {
-
+fun ContadorAlPresionar() {
+    var conteo by remember{ mutableStateOf(0) }
+    //Modifier me sirbe para agregarle nuevos atributos al elemento
+    Text(text = "Conteo: $conteo".toString(),
+        Modifier
+            .clickable { conteo++ })
 }
-
 @Preview(showBackground = true)
 @Composable
 fun GreetingPreview() {
     JetpackCompuseTheme {
-        Greeting()
+        ContadorAlPresionar()
     }
 }
